6727fa3986eb6ccab6af1fbaca907a96c9251ed2
Alex LaFroscia <alex@lafroscia.com>
2018-07-06 10:28:59 -0700

    chore(deps): upgrade linting plugins

--- packages/ember-cli-stencil/.eslintrc.js
+++ packages/ember-cli-stencil/.eslintrc.js
@@ -4,22 +4,13 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  plugins: [
-    'ember',
-    'prettier'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'prettier'
-  ],
+  plugins: ['ember', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
   env: {
     browser: true
   },
   rules: {
-    'prettier/prettier': ['error', {
-      singleQuote: true
-    }]
+    'prettier/prettier': 'error'
   },
   overrides: [
     // node files
@@ -48,9 +39,13 @@ module.exports = {
         node: true
       },
       plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      })
+      rules: Object.assign(
+        {},
+        require('eslint-plugin-node').configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+        }
+      )
     }
   ]
 };
