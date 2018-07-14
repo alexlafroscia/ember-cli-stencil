d2db8c00f280e156450a58885f298308f909b59a
Alex LaFroscia <alex@lafroscia.com>
2018-07-14 16:58:34 -0700

    fix: import Stencil packages through ESM
    Now imports Stencil packages through ESM rather than the “normal” browser build
    
    BREAKING CHANGE: User must now manually import and define Stencil collections, as they would with other build tools. This is due to not being able to find a way to make the initialization implicit while bundling the files through `ember-auto-import`.

--- packages/ember-cli-stencil/tests/dummy/app/app.js
+++ packages/ember-cli-stencil/tests/dummy/app/app.js
@@ -4,6 +4,9 @@ import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import CustomEvents from 'ember-cli-stencil/mixins/custom-events';
 
+import { defineCustomElements as defineDemoComponents } from 'demo-components';
+defineDemoComponents(window);
+
 const App = Application.extend(CustomEvents, {
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
